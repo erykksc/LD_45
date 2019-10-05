06f95b591c524a1cf338b000962f239aafd26135
Tifix <wertynius2@gmail.com>
2019-10-05 02:25:13 +0200

    created prefabs for cells, added var to controll activationstate

---
+++ ld45/Assets/Scripts/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public bool IsActive;
+        public Sprite SpriteActivated;
+        public Sprite SpriteDeactivated;
+
+
+    public void Start()
+    {
+        StartCoroutine(LoopActivation());
+    }
+
+    public void FixedUpdate()
+    {
+        //Setting sprite accordingly to cell's activation state
+        if (IsActive && gameObject.GetComponent<SpriteRenderer>().sprite == SpriteDeactivated) gameObject.GetComponent<SpriteRenderer>().sprite = SpriteActivated;
+        if (!IsActive && gameObject.GetComponent<SpriteRenderer>().sprite == SpriteActivated) gameObject.GetComponent<SpriteRenderer>().sprite = SpriteDeactivated;
+    }
+
+    // Update is called once per frame
+    IEnumerator LoopActivation()
+    {
+        while (true)
+        {
+            IsActive = !IsActive;
+            Debug.Log("change");
+            yield return new WaitForSeconds(2);
+        }
+
+    }
+}
