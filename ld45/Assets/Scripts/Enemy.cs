4518ab6952b4c9f854d5566f6ffd9a620bb82dbd
Hakier <grzes869@gmail.com>
2019-10-05 12:36:54 +0200

    Turret alpha

--- ld45/Assets/Scripts/Enemy.cs
+++ ld45/Assets/Scripts/Enemy.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log("Collision");
+        //Debug.Log("Collision");
         if (collision.gameObject.CompareTag("Cell"))
         {
             GameObject.Destroy(collision.gameObject);
-            Debug.Log($"{collision.gameObject.name} destroyed");
+            //Debug.Log($"{collision.gameObject.name} destroyed");
         }
     }
 }
