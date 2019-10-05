f4eec600ee642fa29461580a6fb0ee7c6d83e754
andrulonis <andrulonis@spoko.pl>
2019-10-05 17:52:40 +0200

    vgcfb

--- ld45/Assets/Scripts/CameraAtStart.cs
+++ ld45/Assets/Scripts/CameraAtStart.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class CameraAtStart : MonoBehaviour
 {
+    public CellFactory factory;
     // Start is called before the first frame update
     void Start()
     {
-        
         transform.position = new Vector3(26, 13,-3);
+        factory.Add(Cell.getHexCoords(new Vector2(26, 13),1));
+    }
 
-
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
