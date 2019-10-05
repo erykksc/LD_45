36127bf0347c49174300a09c524e5bf92c604b14
Tifix <wertynius2@gmail.com>
2019-10-06 00:03:27 +0200

    CameraMovement
    WSAD and ArrowKeys

---
+++ ld45/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.down * MoveSpeed);
+        }
+
+
+    }
+}
