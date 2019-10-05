9a71afb13ae6f22b48992c1f30db62e2cbaf3983
Tifix <wertynius2@gmail.com>
2019-10-05 19:44:31 +0200

    particle effect prefab added

---
+++ ld45/Assets/Scripts/PSDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSDestroyer : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gameObject.GetComponent<ParticleSystem>().particleCount == 0) Destroy(gameObject);
+
+    }
+}
