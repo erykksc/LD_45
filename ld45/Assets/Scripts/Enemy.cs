3862dfc2e9f29274473b4ea4626d6847153c8f98
Eroar <erykksc@gmail.com>
2019-10-05 12:45:32 +0200

    Added hp and dealDamage to Enemy

--- ld45/Assets/Scripts/Enemy.cs
+++ ld45/Assets/Scripts/Enemy.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int damage;
+    [SerializeField] private int hp;
 
     private void Start() {
         damage = 30;
     }
+
+    public void dealDamage(int damage2deal)
+    {
+        if (damage > 0)
+        {
+            hp -= damage;
+        }
+
+        if (hp <= 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Cell"))
         {
             collision.gameObject.GetComponent<Cell>().dealDamage(damage);
-            // GameObject.Destroy(collision.gameObject);
+            // c
             // Debug.Log($"{collision.gameObject.name} destroyed");
         }
     }
