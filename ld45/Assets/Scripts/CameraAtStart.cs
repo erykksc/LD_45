08c4a7f9e3d7f30c144ac4bf3ae1b496745162a5
andrulonis <andrulonis@spoko.pl>
2019-10-05 19:22:43 +0200

    Revert " vdfbxc"
    This reverts commit e2894a7ff375caa445b9de8a262b2f0c17422d1d.

--- ld45/Assets/Scripts/CameraAtStart.cs
+++ ld45/Assets/Scripts/CameraAtStart.cs
@@ -6,29 +6,10 @@ public class CameraAtStart : MonoBehaviour
 {
     public CellFactory factory;
     // Start is called before the first frame update
-    private int[] a;
-    
     void Start()
     {
-        int []a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        Random ran = new Random();
-        int mynum = a[Random.Range(1, 11)];
-        //[ran.Next(0, a.Length];
-        if (mynum < 9)
-        {
-            int b = 1;
-            if (mynum == 9)
-            {
-                b = 2;
-
-            }
-            else
-            {
-                b = 3;
-            }
-        }
         transform.position = new Vector3(26, 13,-3);
-        factory.Add(Cell.getHexCoords(new Vector2(26, 13),1), mynum);
+        factory.Add(Cell.getHexCoords(new Vector2(26, 13),1),1);
         ScoreCore.mainSpawner = GameObject.FindGameObjectWithTag("MainSpawner");
     }
 
