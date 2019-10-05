70afa94d9c746cd2b07099cf132697bce4f6bc61
user31415926 <kamilkowalski212@gmail.com>
2019-10-05 23:39:57 +0200

    Revert "Merge branch 'master' of https://github.com/Eroar/LD_45"
    This reverts commit 0c819be6a5abd90b19f408905e0444799042d18d, reversing
    changes made to 44625714c3e5b3e6d42293cb5e9a453688b3032f.

--- ld45/Assets/Scripts/CameraAtStart.cs
+++ ld45/Assets/Scripts/CameraAtStart.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CameraAtStart : MonoBehaviour
 {
-    public MapGenerator Mapgen;
-    //public MapGenerator ySize;
     public CellFactory factory;
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.LogWarning((xSize.xSize * 0.5f + 0.5f).ToString()+"/ "+ (ySize.ySize * 0.5f + 0.5f).ToString());
-        transform.position = new Vector3(Mapgen.xSize * 0.5f + 0.5f, Mapgen.ySize * 0.5f + 0.5f, -3);
-        factory.Add(Cell.getHexCoords(new Vector2(Mapgen.xSize*0.5f+0.5f, Mapgen.ySize*0.5f+1.5f),1),1);
+        transform.position = new Vector3(26, 13,-3);
+        factory.Add(Cell.getHexCoords(new Vector2(26, 13),1),1);
         ScoreCore.mainSpawner = GameObject.FindGameObjectWithTag("MainSpawner");
     }
 
@@ -21,9 +18,4 @@ public class CameraAtStart : MonoBehaviour
     {
         
     }
-    private void OnDrawGizmos()
-    {
-        //Gizmos.DrawSphere(new Vector3((Mapgen.xSize * 0.5f + 0.5f),( Mapgen.ySize * 0.5f + 0.5f), -3),        4); 
-    }
-
 }
