d40720547324cd179b58b7f71712ebc40ea2a677
andrulonis <andrulonis@spoko.pl>
2019-10-05 16:06:13 +0200

    n hjtmyrcfdt v

---
+++ ld45/Assets/Scripts/CameraAtStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAtStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(26, 13,-3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
