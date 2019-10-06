6a2c74685965c1b90d522e92a50c0969b42c18b0
Eroar <erykksc@gmail.com>
2019-10-06 18:36:20 +0200

    Working scene

--- ld45/Assets/Scripts/ScoreCore.cs
+++ ld45/Assets/Scripts/ScoreCore.cs
@@ -84,7 +84,7 @@ public class ScoreCore : MonoBehaviour
         {
             nextNumOfEnemiesGroups += 1;
         }
-        Debug.Log($"Wave enemies {round} ,{nextNumOfEnemiesGroups}, {nextNumOfEnemiesPerGroup}");
+        Debug.Log($"Wave enemies {round+1} ,{nextNumOfEnemiesGroups}, {nextNumOfEnemiesPerGroup}");
     }
 
     private void FixedUpdate() {
