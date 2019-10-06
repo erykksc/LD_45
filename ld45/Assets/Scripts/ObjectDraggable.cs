b4fbf3cb9de28c08eac238d907e7aca6fe4f71ce
user31415926 <kamilkowalski212@gmail.com>
2019-10-06 21:51:39 +0200

    5 times the charm

--- ld45/Assets/Scripts/ObjectDraggable.cs
+++ ld45/Assets/Scripts/ObjectDraggable.cs
@@ -6,6 +6,7 @@ public class ObjectDraggable : MonoBehaviour
 {
     public bool IsSelected = false;
     public CellFactory Factory;
+    public CellFactory grassFactory;
     public int SpawnedIdentifier=0;
     public Vector3 ReturnPosition;
 
@@ -46,7 +47,7 @@ public class ObjectDraggable : MonoBehaviour
 
                 
             //Checking if position is occupied and if player has enough cash to build the cell
-            if ((Factory.Find(hPos) == null&&ScoreCore.Cash>= ScoreCore.Prices[SpawnedIdentifier])||makeInfinite)
+            if ((Factory.Find(hPos) == null&&ScoreCore.Cash>= ScoreCore.Prices[SpawnedIdentifier]&&grassFactory.Find(hPos).buildable)
             {
                 Factory.Add(hPos, SpawnedIdentifier);
                 GameObject.Instantiate(Resources.Load<GameObject>("BuildParticles") as GameObject, Cell.getGlobalCoords(Cell.getHexCoords(WorldPos, 55f/64f), 55f/64f), Quaternion.identity);
