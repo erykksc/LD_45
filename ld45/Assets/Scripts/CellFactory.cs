1c3b23d34afe144808b0f2f34c392dff26414572
user31415926 <kamilkowalski212@gmail.com>
2019-10-06 19:51:56 +0200

    Cell fix

--- ld45/Assets/Scripts/CellFactory.cs
+++ ld45/Assets/Scripts/CellFactory.cs
@@ -34,9 +34,11 @@ public class CellFactory : MonoBehaviour
         {
             return null;
         }
-        o = Instantiate(cellPrefabs[index]);
+        o = Instantiate(cellPrefabs[index]  );
+        
         o.Instantiate(pos);
         o.name = cellPrefabs[index].name;
+        
         for (int i = 0; i < cells.Count; i++)
         {
 
