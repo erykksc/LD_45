49e746ada31629ceaca0899e8677d61d0c76c166
Hakier <grzes869@gmail.com>
2019-10-08 01:13:32 +0200

    x

--- ld45/Assets/Scripts/Silos.cs
+++ ld45/Assets/Scripts/Silos.cs
@@ -37,4 +37,34 @@ public class Silos : Cell
     {
         
     }
+    private void OnDestroy()
+    {
+        allInst.Remove(this);
+        if(ID>=0)
+        {
+            ScoreCore.cellCount[ID]--;
+        }
+        
+        ScoreCore.Prices[0] = 4 * ScoreCore.cellCount[0];
+        ScoreCore.Prices[2] = 4 * ScoreCore.cellCount[2];
+        ScoreCore.Prices[3] = 4 * ScoreCore.cellCount[3];
+        ScoreCore.Prices[4] = 4 * ScoreCore.cellCount[4];
+        Camera.main.GetComponent<ScoreCore>().PriceDisplayers[0].text = ScoreCore.Prices[0].ToString() + "$";
+        Camera.main.GetComponent<ScoreCore>().PriceDisplayers[2].text = ScoreCore.Prices[2].ToString() + "$";
+        Camera.main.GetComponent<ScoreCore>().PriceDisplayers[3].text = ScoreCore.Prices[3].ToString() + "$";
+        Camera.main.GetComponent<ScoreCore>().PriceDisplayers[4].text = ScoreCore.Prices[4].ToString() + "$";
+
+        CellFactory CF = null;
+        Transform SemiTarget = null;
+        foreach (Transform trans in Camera.main.GetComponentsInChildren<Transform>())
+        {
+            if (trans.gameObject.name == "Canvas") SemiTarget = trans;
+        }
+        foreach (Transform trans in Camera.main.GetComponentsInChildren<Transform>())
+        {
+            if (trans.gameObject.name == "GrassFactory") SemiTarget = trans;
+        }
+        CF = SemiTarget.GetComponent<CellFactory>();
+        CellFactory.cellCount--;
+    }
 }
