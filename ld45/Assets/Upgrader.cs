1d22ae66103d781786a6a7a63699613a3da963fa
Tifix <wertynius2@gmail.com>
2019-10-07 18:17:05 +0200

    Upgrade, Destroy UI functionality

--- ld45/Assets/Upgrader.cs
+++ ld45/Assets/Upgrader.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class Upgrader : MonoBehaviour
 {
+    [SerializeField]  private  CellFactory CF;
+
+    public void Awake()
+    {
+        CF = null;
+        Transform SemiTarget = null;
+        
+        //Getting access to the CellFactory without .Find
+        foreach (Transform trans in Camera.main.GetComponentsInChildren<Transform>())
+        {
+            if (trans.gameObject.name == "Canvas") SemiTarget = trans;
+        }
+        foreach (Transform trans in Camera.main.GetComponentsInChildren<Transform>())
+        {
+            if (trans.gameObject.name == "CellFactory") SemiTarget = trans;
+        }
+        CF = SemiTarget.GetComponent<CellFactory>();
+
+
+
+    }
+
     public void Upgrade()
     {
+        CF.Find(Cell.getHexCoords(transform.position, 55f / 64f)).Upgrade();
         Debug.LogError("Upgrade");
     }
 
 
     public void Bulldoze()
     {
+        CF.DestroyCell(Cell.getHexCoords(transform.position, 55f / 64f)    );    
         Debug.LogError("Downgrade");
+    }
+    private void FixedUpdate()
+    {
+        if (CF.Find(Cell.getHexCoords(transform.position, 55f / 64f))==null) Destroy(gameObject);
     }
 }
